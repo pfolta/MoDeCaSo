73738f48ee36ea1d0f8ca0cacc460cd414a7530a
Peter Folta <mail@peterfolta.net>
2014-10-18 09:16:55 +0000

    Created top level index.php to redirect client to frontend

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * UPB-BTHESIS
+ * Copyright (C) 2004-2014 Peter Folta. All rights reserved.
+ *
+ * Project:			UPB-BTHESIS
+ * Version:			0.0.1
+ *
+ * File:			/index.php
+ * Created:			2014-10-18
+ * Last modified:	2014-10-18
+ * Author:			Peter Folta <mail@peterfolta.net>
+ */
+
+/*
+ * Redirect client to frontend
+ */
+header("Location: frontend/#");
+exit;
