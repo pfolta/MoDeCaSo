f91e16728de868e8b7e72cac047750943b41e89b
Peter Folta <mail@peterfolta.net>
2014-11-11 09:23:24 +0000

    Added try catch block

--- server/index.php
+++ server/index.php
@@ -9,7 +9,7 @@
  *
  * File:			/server/index.php
  * Created:			2014-11-03
- * Last modified:	2014-11-10
+ * Last modified:	2014-11-11
  * Author:			Peter Folta <mail@peterfolta.net>
  */
 
@@ -32,56 +32,70 @@ use classes\errorhandling;
 
 use controllers\auth_controller;
 
-/*
- * Create new instance of web application
- */
-$app = new Slim();
-$app->view(new JsonApiView());
-$app->add(new JsonApiMiddleware());
+try {
+    /*
+     * Create new instance of web application
+     */
+    $app = new Slim();
+    $app->view(new JsonApiView());
+    $app->add(new JsonApiMiddleware());
 
-/*
- * Initialize config object, load and parse config file
- */
-$config = config::get_instance();
-$config->load_config("config.ini");
+    /*
+     * Initialize config object, load and parse config file
+     */
+    $config = config::get_instance();
+    $config->load_config("config.ini");
 
-/*
- * Set debug options
- */
-if ($config->get_config_value("main", "debug")) {
-    errorhandling::set_error_handling(true);
-    $app->config("debug", true);
-    $app->config("mode", "debug");
-} else {
-    errorhandling::set_error_handling(false);
-    $app->config("debug", false);
-    $app->config("mode", "production");
+    /*
+     * Set debug options
+     */
+    if ($config->get_config_value("main", "debug")) {
+        errorhandling::set_error_handling(true);
+        $app->config("debug", true);
+        $app->config("mode", "debug");
+    } else {
+        errorhandling::set_error_handling(false);
+        $app->config("debug", false);
+        $app->config("mode", "production");
+    }
+
+    /*
+     * Initialize database object, connect with credentials provided in config file
+     */
+    $database = database::get_instance();
+    $database->connect(
+        $config->get_config_value("database", "server"),
+        $config->get_config_value("database", "username"),
+        $config->get_config_value("database", "password"),
+        $config->get_config_value("database", "database")
+    );
+
+    /*
+     * Set database connection character set
+     */
+    $database->set_charset("UTF8");
+
+    $auth = auth::get_instance();
+
+    /*
+     * Instantiate controllers
+     */
+    $auth_controller = new auth_controller();
+
+    /*
+     * Finally, handle requests
+     */
+    $app->run();
+} catch (Exception $exception) {
+    header($_SERVER['SERVER_PROTOCOL']." 500 Internal Server Error");
+    header("Content-Type: application/json");
+
+    $error = array(
+        'error'     => true,
+        'msg'       => "internal_application_error",
+        'reason'    => $exception->getMessage(),
+        'status'    => 500
+    );
+
+    print json_encode($error);
 }
-
-/*
- * Initialize database object, connect with credentials provided in config file
- */
-$database = database::get_instance();
-$database->connect(
-    $config->get_config_value("database", "server"),
-    $config->get_config_value("database", "username"),
-    $config->get_config_value("database", "password"),
-    $config->get_config_value("database", "database")
-);
-
-/*
- * Set database connection character set
- */
-$database->set_charset("UTF8");
-
-$auth = auth::get_instance();
-
-/*
- * Instantiate controllers
- */
-$auth_controller = new auth_controller();
-
-/*
- * Finally, handle requests
- */
-$app->run();
