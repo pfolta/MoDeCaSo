6e2977535de245c0eb22bcc19760c6690a3d6204
Peter Folta <mail@peterfolta.net>
2014-12-17 17:40:25 +0000

    Added edit card controller

---
+++ frontend/js/app/controllers/projects/edit_card.js
@@ -0,0 +1,132 @@
+/*
+ * UPB-BTHESIS
+ * Copyright (C) 2004-2014 Peter Folta. All rights reserved.
+ *
+ * Project:			UPB-BTHESIS
+ * Version:			0.0.1
+ *
+ * File:            /frontend/js/app/controllers/projects/edit_card.js
+ * Created:			2014-12-17
+ * Last modified:	2014-12-17
+ * Author:			Peter Folta <pfolta@mail.uni-paderborn.de>
+ */
+
+controllers.controller(
+    "edit_card_controller",
+    [
+        "$scope",
+        "$rootScope",
+        "$http",
+        "session_service",
+        "card_id",
+        function($scope, $rootScope, $http, session_service, card_id)
+        {
+            $scope.card_id = card_id;
+
+            $scope.flash = {
+                "show":     false,
+                "type":     null,
+                "message":  null
+            };
+
+            $scope.load_card = function(card_id)
+            {
+                $scope.card = null;
+
+                $http({
+                    method:     "get",
+                    url:        "/server/projects/cards/get_card/" + card_id,
+                    headers:    {
+                        "X-API-Key":    session_service.get("api_key")
+                    }
+                }).then(
+                    function(response)
+                    {
+                        $scope.flash.show = false;
+
+                        $scope.card = response.data.card;
+                    },
+                    function(response)
+                    {
+                        $scope.flash.show = true;
+                        $scope.flash.type = "alert-danger";
+                        $scope.flash.message = "<span class='glyphicon glyphicon-exclamation-sign'></span> <strong>" + get_error_title() + "</strong> Error loading card.";
+
+                        shake_element($("#edit_card_flash"));
+                    }
+                );
+            };
+
+            $scope.edit_card = function()
+            {
+                /*
+                 * Disable form elements to prevent duplicate requests
+                 */
+                $("#edit_card_submit_button").prop("disabled", true);
+                $("#edit_card_cancel_button").prop("disabled", true);
+
+                $http({
+                    method:     "post",
+                    url:        "/server/projects/cards/edit_card",
+                    data:       {
+                        card_id:    $scope.card.id,
+                        value:      $scope.card.value
+                    },
+                    headers:    {
+                        "X-API-Key":    session_service.get("api_key")
+                    }
+                }).then(
+                    function(response)
+                    {
+                        /*
+                         * Enable form elements
+                         */
+                        $("#edit_card_submit_button").prop("disabled", false);
+                        $("#edit_card_cancel_button").prop("disabled", false);
+
+                        $scope.flash.show = true;
+                        $scope.flash.type = "alert-success";
+                        $scope.flash.message = "<span class='glyphicon glyphicon-ok-sign'></span> <strong>" + get_success_title() + "</strong> The card has been successfully updated.";
+
+                        /*
+                         * Disable submit button and change Cancel button to show "Close" instead
+                         */
+                        $("#edit_card_submit_button").prop("disabled", true);
+                        $("#edit_card_cancel_button").html("Close");
+
+                        $("#edit_card_cancel_button").on(
+                            "click",
+                            function()
+                            {
+                                $rootScope.$broadcast("load_project");
+                            }
+                        );
+                        $("#edit_card_close_button").on(
+                            "click",
+                            function()
+                            {
+                                $rootScope.$broadcast("load_project");
+                            }
+                        );
+                    },
+                    function(response)
+                    {
+                        /*
+                         * Enable form elements
+                         */
+                        $("#edit_card_submit_button").prop("disabled", false);
+                        $("#edit_card_cancel_button").prop("disabled", false);
+
+                        $scope.flash.show = true;
+                        $scope.flash.type = "alert-danger";
+                        $scope.flash.message = "<span class='glyphicon glyphicon-exclamation-sign'></span> <strong>" + get_error_title() + "</strong> The card could not be updated.";
+
+                        shake_element($("#edit_card_flash"));
+                    }
+                );
+            };
+
+            $scope.load_card($scope.card_id);
+        }
+    ]
+);
