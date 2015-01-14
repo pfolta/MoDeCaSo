d59ef2b13574cb6cc7be2cb8cd763ee26b3f2741
Peter Folta <mail@peterfolta.net>
2015-01-14 21:02:01 +0000

    Added participant statuses for adding new participants manually

--- server/model/projects/participants.class.php
+++ server/model/projects/participants.class.php
@@ -51,6 +51,7 @@ class participants
             'first_name'    => $first_name,
             'last_name'     => $last_name,
             'email'         => $email,
+            'status'        => participant_statuses::ADDED,
             'created'       => time(),
             'last_modified' => time()
         ));
