2ae65cade1656b31a9d006b541d60c5b877b21bb
Peter Folta <mail@peterfolta.net>
2015-03-09 11:08:54 +0000

    Updated service

--- server/server_svc.php
+++ server/server_svc.php
@@ -37,100 +37,127 @@ require "model/projects/participants.class.php";
 require "model/projects/projects.class.php";
 require "model/auth.class.php";
 
+use data\participant_statuses;
 use data\project_statuses;
 use main\config;
 use main\database;
 use main\errorhandling;
 
-/*
- * Print welcome message
- */
-print(
-"
-MoDeCaSo - A Web Application for Modified Delphi Card Sorting Experiments
-Server Maintenance Service
+try {
+    /*
+     * Print welcome message
+     */
+    print("\nMoDeCaSo - A Web Application for Modified Delphi Card Sorting Experiments\nServer Maintenance Service\n\n");
 
-");
+    /*
+     * Check if service is run from command line (terminal)
+     */
+    if (php_sapi_name() != "cli") {
+        die("Please run me from the console - not from a web browser!");
+    }
 
-/*
- * Check if service is run from command line (terminal)
- */
-if (php_sapi_name() != "cli") {
-    die("Please run me from the console - not from a web browser!");
+    /*
+     * Print start timestamp
+     */
+    print("Process Started: " . date("n/j/Y g:i:s A") . "\n\n\n");
+
+    /*
+     * Initialize global timestamp variable
+     */
+    $timestamp = time();
+
+    /*
+     * Initialize config object, load and parse config file
+     */
+    print("Loading and parsing config file...\t\t\t\t\t");
+    $config = config::get_instance();
+    $config->load_config("config.ini");
+    print("[Done]\n");
+
+    /*
+     * Set debug options
+     */
+    print("Setting debug options...\t\t\t\t\t\t");
+    if ($config->get_config_value("main", "debug")) {
+        errorhandling::set_error_handling(true);
+    } else {
+        errorhandling::set_error_handling(false);
+    }
+    print("[Done]\n");
+
+    /*
+     * Initialize database object, connect with credentials provided in config file
+     */
+    print("Connecting to database...\t\t\t\t\t\t");
+    $database = database::get_instance();
+    $database->connect(
+        $config->get_config_value("database", "server"),
+        $config->get_config_value("database", "username"),
+        $config->get_config_value("database", "password"),
+        $config->get_config_value("database", "database")
+    );
+
+    /*
+     * Set database connection character set
+     */
+    $database->set_charset("UTF8");
+    print("[Done]\n");
+
+    /*
+     * Print project list
+     */
+    print("\n\nPROJECT LIST\n\n");
+
+    $projects = $database->select("projects", null);
+    $projects = $database->result();
+
+    $mask = "%5.5s     %-40.40s     %-10.10s     %-8.8s\n";
+    printf($mask, "ID", "Title", "Key", "Status");
+    print("------------------------------------------------------------------------------\n");
+
+    for ($i = 0; $i < count($projects); $i++) {
+        printf($mask, $projects[$i]['id'], $projects[$i]['title'], $projects[$i]['key'], project_statuses::$values[$projects[$i]['status']]);
+    }
+
+    /*
+     * Servicing projects
+     */
+    print("\n\nSERVICING PROJECTS...\n\n");
+
+    for ($i = 0; $i < count($projects); $i++) {
+        print("Servicing Project [".$projects[$i]['id'].": ".$projects[$i]['key']."]\n");
+
+        switch($projects[$i]['status']) {
+            case project_statuses::RUNNING:
+                print("Project is running.\n");
+
+                /*
+                 * Print participant list
+                 */
+                print("\n\nPARTICIPANT LIST\n\n");
+
+                $participants = $database->select("project_participants", null);
+                $participants = $database->result();
+
+                $mask = "%5.5s     %-20.20s     %-20.20s     %-8.8s\n";
+                printf($mask, "ID", "First Name", "Last Name", "Status");
+                print("------------------------------------------------------------------------------\n");
+
+                for ($i = 0; $i < count($participants); $i++) {
+                    printf($mask, $participants[$i]['id'], $participants[$i]['first_name'], $participants[$i]['last_name'], participant_statuses::$values[$participants[$i]['status']]);
+                }
+
+                break;
+            default:
+                print("Project is not currently running. No action needed.\n\n");
+                break;
+        }
+    }
+
+    /*
+     * Print end timestamp
+     */
+    print("\n\nProcess Finished: " . date("n/j/Y g:i:s A") . "\n");
+} catch (Exception $exception) {
+    print("\n\nA fatal error has occured: ".$exception->getMessage()."\n");
 }
-
-/*
- * Print start timestamp
- */
-print(
-    "Process Started: ".date("n/j/Y g:i:s A")."
-
-");
-
-/*
- * Initialize global timestamp variable
- */
-$timestamp = time();
-
-/*
- * Initialize config object, load and parse config file
- */
-$config = config::get_instance();
-$config->load_config("config.ini");
-
-/*
- * Set debug options
- */
-if ($config->get_config_value("main", "debug")) {
-    errorhandling::set_error_handling(true);
-} else {
-    errorhandling::set_error_handling(false);
-}
-
-/*
- * Initialize database object, connect with credentials provided in config file
- */
-$database = database::get_instance();
-$database->connect(
-    $config->get_config_value("database", "server"),
-    $config->get_config_value("database", "username"),
-    $config->get_config_value("database", "password"),
-    $config->get_config_value("database", "database")
-);
-
-/*
- * Set database connection character set
- */
-$database->set_charset("UTF8");
-
-/*
- * Print project list
- */
-print(
-    "Projects:
-
-");
-
-$projects = $database->select("projects", null);
-$projects = $database->result();
-
-$mask = "%5.5s     %-40.40s     %-10.10s     %-8.8s\n";
-printf($mask, "ID", "Title", "Key", "Status");
-print("------------------------------------------------------------------------------
-");
-
-for ($i = 0; $i < count($projects); $i++) {
-    printf($mask, $projects[$i]['id'], $projects[$i]['title'], $projects[$i]['key'], project_statuses::$values[$projects[$i]['status']]);
-}
-
-print("
-");
-
-/*
- * Print end timestamp
- */
-print(
-    "Process Finished: ".date("n/j/Y g:i:s A")."
-
-"
-);
