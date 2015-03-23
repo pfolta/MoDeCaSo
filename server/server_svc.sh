e1c2b2593cc2b2b65a8b0ea9a7d673af04996b0b
Peter Folta <mail@peterfolta.net>
2015-03-23 08:55:51 +0000

    Added shell script for server service script

---
+++ server/server_svc.sh
@@ -0,0 +1,16 @@
+#
+# UPB-BTHESIS
+# Copyright (C) 2014-2015 Peter Folta. All rights reserved.
+#
+# Project:			UPB-BTHESIS
+# Version:			0.0.1
+#
+# File:			    /server/server_svc.sh
+# Created:			2015-03-23
+# Last modified:	2015-03-23
+# Author:			Peter Folta <pfolta@mail.uni-paderborn.de>
+#
+
+#!/bin/sh
+
+php server_svc.php
